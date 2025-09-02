e169f67ff23dbe47ff1dc25b540f3c0abeebaafd
saikey <gintama10283291@gmail.com>
2025-09-02 12:22:33 +0900

    여정인

---
+++ unittest/basic_funcs.py
@@ -0,0 +1,12 @@
+def even_def(a):
+    if a%2 == 0 : return True
+    else : return False
+
+def avg_def(a):
+    return (sum(a) / len(a))
+
+def min_def(a):
+    return min(a)
+
+def max_def(a):
+    return max(a)
--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,20 @@
-# TODO: 사용자 모듈 import
+from basic_funcs import even_def, avg_def, min_def, max_def
 
+def test_even():
+    assert True == even_def(8)
+    assert False == even_def(7)
 
-# TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+def test_avg():
+    a = [1, 3]
+    assert isinstance(a, list)
+    assert 2 == avg_def(a)
+
+def test_min():
+    a = [1, 10, 500]
+    assert isinstance(a, list)
+    assert 1 == min_def(a)
+
+def test_max():
+    a = [1, 10, 500]
+    assert isinstance(a, list)
+    assert 500 == max_def(a)
